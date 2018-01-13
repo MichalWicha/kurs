085b38e02bb50c883331455609011509759278a6
MichalWicha <34377481+MichalWicha@users.noreply.github.com>
2018-01-13 19:39:48 +0100

    Add files via upload

---
+++ 2_TEB/5_kolory/kolory.js
@@ -0,0 +1,33 @@
+var elPrzycisk = document.getElementById("przycisk");
+var elCzerwony = document.getElementById("czerwony");
+var elZielony = document.getElementById ("zielony");
+var elOrange = document.getElementById ("orange");
+var elInny = document.getElementById ("inny");
+var elWynik = document.getElementById("wynik");
+var elWybierzKolor = document.getElementById("WybierzKolor");
+elWybierzKolor.style.visibility = "hidden";
+var innyKolor
+elPrzycisk.onclick = function(){
+    //alert("test");
+    if(elCzerwony.checked){
+        elWynik.innerHTML = "Twój ulubiony kolor: czerwony";
+        
+    }else if(elZielony.checked){
+        elWynik.innerHTML = "Twój ulubiony kolor: zielony";
+        
+    }else if(elOrange.checked){
+        elWynik.innerHTML = "Twoój ulubiony kolor: orange";
+        
+    }else{
+       // console.log(elWybierzKolor.value);
+        innyKolor = elWybierzKolor.value;
+        elWynik.innerHTML= "Twój ulubiony kolor: " + innyKolor;
+    
+    }
+    
+} 
+
+elInny.onclick = function(){
+    elWynik.innerHTML = "wybierz kolor";
+    elWybierzKolor.style.visibility = "visible";
+}
---
+++ 2_TEB/5_kolory/kolory1.js
@@ -0,0 +1,43 @@
+var elPrzycisk = document.getElementById("przycisk");
+var elCzerwony = document.getElementById("czerwony");
+var elZielony = document.getElementById ("zielony");
+var elOrange = document.getElementById ("orange");
+var elInny = document.getElementById ("inny");
+var elWynik = document.getElementById("wynik");
+var elWynikKolor = document.getElementById("wynikKolor");
+var elWybierzKolor = document.getElementById("WybierzKolor");
+elWybierzKolor.style.visibility = "hidden";
+var innyKolor
+elPrzycisk.onclick = function(){
+    //alert("test");
+    if(elCzerwony.checked){
+        elWynik.innerHTML = "Twój ulubiony kolor: ";
+        elWynikKolor.innerHTML = "czerwony";
+        elWynikKolor.style.color ="red";
+        
+    }else if(elZielony.checked){
+        elWynik.innerHTML = "Twój ulubiony kolor: ";
+        elWynikKolor.innerHTML = "zielony";
+        elWynikKolor.style.color ="green";
+        
+    }else if(elOrange.checked){
+        elWynik.innerHTML = "Twój ulubiony kolor: ";
+        elWynikKolor.innerHTML = "orange";
+        elWynikKolor.style.color ="orange";
+        
+
+        
+}else{
+       // console.log(elWybierzKolor.value);
+        innyKolor = elWybierzKolor.value;
+        elWynik.innerHTML= "Twój ulubiony kolor: " + innyKolor;
+    
+
+    }
+}
+ 
+
+elInny.onclick = function(){
+    elWynik.innerHTML = "wybierz kolor";
+    elWybierzKolor.style.visibility = "visible";
+}
